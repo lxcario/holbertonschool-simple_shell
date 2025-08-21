965e4ca64f876dfebcb178df45d7d5ce085cdf96
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-08-21 21:26:08 +0300

    yes

--- shell.c
+++ shell.c
@@ -1,53 +1,72 @@
-#include <stdio.h>
+'#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 /**
- * main - Simple command-line interpreter
+ * main - Simple shell entry point
+ * @void: No parameters
  *
- * Return: 0 on success
+ * Return: 0 on success.
  */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	extern char **environ;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    pid_t pid;
+    int status;
+    char *argv[2];
+    extern char **environ;
 
-	while (1)
-	{
-		printf("#resqueshell$ ");
-		read = getline(&line, &len, stdin);
-		if (read == -1)
-		{
-			printf("\n");
-			break;
-		}
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
-		if (line[0] == '\0')
-			continue;
-		pid_t pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			continue;
-		}
-		if (pid == 0)
-		{
-			char *argv[] = {line, NULL};
-			execve(line, argv, environ);
-			fprintf(stderr, "Error: %s not found\n", line);
-			free(line);
-			exit(1);
-		}
-		else
-		{
-			wait(NULL);
-		}
-	}
-	free(line);
-	return 0;
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("#resqueshell$ ");
+
+        read = getline(&line, &len, stdin);
+        
+        if (read == -1)
+        {
+            if (isatty(STDIN_FILENO))
+                printf("\n");
+            break;
+        }
+
+        if (line[read - 1] == '\n')
+            line[read - 1] = '\0';
+
+        if (strlen(line) == 0)
+            continue;
+
+        pid = fork();
+        
+        if (pid == -1)
+        {
+            perror("fork");
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            argv[0] = line;
+            argv[1] = NULL;
+            
+            if (execve(argv[0], argv, environ) == -1)
+            {
+                perror(line);
+                _exit(1);
+            }
+        }
+        else
+        {
+            if (wait(&status) == -1)
+                perror("wait");
+        }
+    }
+
+    free(line);
+    return (0);
 }
